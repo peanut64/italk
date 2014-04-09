75fdf309d8c08bdf14caca48dd67f66b872e9027
peanut64 <64peanuts@gmail.com>
2014-04-09 08:19:18 +0530

    GUI - main window

--- italk/italk.cpp
+++ italk/italk.cpp
@@ -1,1 +1,19 @@
-#include <wx/wx.h>
+#include <gtkmm.h>
+#include <sound.h>
+
+int main(int argc, char *argv[])
+{
+    Gtk::Main kit(argc, argv);
+
+    Gtk::Window window;
+
+    window.set_title("Italk");
+
+    window.set_size_request(500,600);
+
+    window.set_tooltip_text("Select 'Update' from the 'Chat' menu to get list of online friends.");
+
+    Gtk::Main::run(window);
+
+    return 0;
+}
